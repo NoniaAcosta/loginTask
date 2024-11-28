3f2b8ce2dc6dd0358bd132d9b94dea8affa6af31
Nonia Isabel Acosta Britos <noniaacosta97@gmail.com>
2024-11-28 08:03:03 -0300

    editando

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
+
+
 urlpatterns = [
     path('signup/', views.UserCreateAndLoginView.as_view(), name='signup'),
     path('login/', LoginView.as_view(
--- loginTask/settings.py
+++ loginTask/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'tasks.middleware.auth.AuthMiddleware',
 ]
 
 ROOT_URLCONF = 'loginTask.urls'
